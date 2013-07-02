83621d028716ebd4970f5339016c41dbce86dd7c
Sakthi Priyan H <sakthipriyans@gmail.com>
2013-07-02 21:15:49 +0530

    Added settings Activity

--- src/com/sakthipriyan/cricscore/MainActivity.java
+++ src/com/sakthipriyan/cricscore/MainActivity.java
@@ -1,23 +1,42 @@
 package com.sakthipriyan.cricscore;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 
+public class MainActivity extends SherlockActivity {
+	
+	// For local reference
+	private Context cxt;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		cxt = this;
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    	MenuInflater inflater = getSupportMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
     }
     
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.settings:
+			startActivity(new Intent(cxt, Settings.class));
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 }
---
+++ src/com/sakthipriyan/cricscore/Settings.java
@@ -0,0 +1,14 @@
+package com.sakthipriyan.cricscore;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Settings extends PreferenceActivity {
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
+	}
+}
