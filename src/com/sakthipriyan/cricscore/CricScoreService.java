0c421241ee4cfa8113087a8cca4317872fad2b39
Sakthi Priyan H <sakthipriyans@gmail.com>
2013-07-03 23:56:01 +0530

    Improved CricSCoreService to provide binder and Main Activity to use it.

--- src/com/sakthipriyan/cricscore/CricScoreService.java
+++ src/com/sakthipriyan/cricscore/CricScoreService.java
@@ -1,46 +1,121 @@
 package com.sakthipriyan.cricscore;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Binder;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
-public class CricScoreService extends Service{
+public class CricScoreService extends Service {
+
+	private static final String TAG = CricScoreService.class.toString();
+	private Date lastModified;
+	private List<Integer> matches;
+	private List<Score> scoresChanged;
+	private List<Score> listMatches;
+	private Timer timer;
+	private boolean checkboxNotify;
+	private int updateInterval;
+	private IBinder binder;
+	private BackEnd backEnd;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Log.d(TAG, "onCreate");
+		this.timer = new Timer("scoreUpdateTimer");
+		this.binder = new LocalBinder();
+		this.backEnd = BackEnd.getInstance();
+		readPreferences();
+	}
+
+	private void readPreferences() {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		this.checkboxNotify = prefs.getBoolean("checkboxNotify", true);
+		this.updateInterval = prefs.getInt("refreshTime", 20000);
+		Log.d(TAG, "Read Preferences. checkboxNotify:" + checkboxNotify
+				+ " updateInterval:" + updateInterval);
+	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
-		return null;
+		Log.d(TAG, "onBind");
+		return this.binder;
 	}
-	
+
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				List<Integer> list = new ArrayList<Integer>();
-				list.add(585683);
-				list.add(593620);
-				Request request = new Request(list,new Date());
-				System.out.println(request);
-				Response response = BackEnd.getInstance().fetchData(request);
-				System.out.println(response);
-				System.out.println(Score.getScores(response.getJson()));
-				list.clear();
-				request = new Request(list,new Date());
-				System.out.println(request);
-				response = BackEnd.getInstance().fetchData(request);
-				System.out.println(response);
-				System.out.println(Score.getScores(response.getJson()));
-			}
-		}).start();
-		
+		Log.d(TAG, "onStartCommand");
+		updateMatchList();
+		background();
 		return Service.START_STICKY;
 	}
 
-	
+	private void updateMatchList() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Log.d(TAG, "updating match list");
+				Response response = backEnd.fetchData(Request.NULL);
+				listMatches = Score.getScores(response.getJson());
+				Log.d(TAG, "updated match list");
+			}
+		}).start();
+	}
+
+	private void background() {
+		Log.d(TAG, "running background()");
+		this.timer.cancel();
+		this.timer = new Timer(true);
+		TimerTask task = new TimerTask() {
+			@Override
+			public void run() {
+				updateScores();
+			}
+		};
+		this.timer.scheduleAtFixedRate(task, 0, this.updateInterval);
+	}
+
+	private void updateScores() {
+		if (this.matches != null && this.matches.size() != 0) {
+			Log.d(TAG, "Running background scores - started");
+			Request request = new Request(this.matches, this.lastModified);
+			Response response = backEnd.fetchData(request);
+			this.scoresChanged = Score.getScores(response.getJson());
+			this.lastModified = response.getLastModified();
+			Log.d(TAG, "Running background scores - ended");
+		}
+	}
+
+	public class LocalBinder extends Binder {
+		CricScoreAPI getService() {
+			return new CricScoreAPI();
+		}
+	}
+
+	public class CricScoreAPI {
+		public List<Score> listMatches() {
+			return CricScoreService.this.listMatches;
+		}
+
+		public List<Score> getScoresChanged() {
+			Log.d(TAG, "getScores(): " + scoresChanged);
+			return scoresChanged;
+		}
+
+		public void setMatches(List<Integer> newMatches) {
+			Log.d(TAG, "setMatches(): " + matches);
+			matches = newMatches;
+			lastModified = null;
+			background();
+		}
+	}
 }
--- src/com/sakthipriyan/cricscore/MainActivity.java
+++ src/com/sakthipriyan/cricscore/MainActivity.java
@@ -1,41 +1,56 @@
 package com.sakthipriyan.cricscore;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 
-import com.actionbarsherlock.app.SherlockListActivity;
+import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
+import com.sakthipriyan.cricscore.CricScoreService.CricScoreAPI;
+import com.sakthipriyan.cricscore.CricScoreService.LocalBinder;
 
-public class MainActivity extends SherlockListActivity {
-	
+public class MainActivity extends SherlockActivity {
+
+	private static final String TAG = MainActivity.class.toString(); 
 	// For local reference
 	private Context cxt;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	private CricScoreAPI cricScoreAPI;
+	private boolean bound = false;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 		cxt = this;
-    }
-    
-    
-    @Override
-    protected void onStart() {
-        super.onStart();
-        startService(new Intent(cxt, CricScoreService.class));
-    }
+		Log.d(TAG, "onCreate");
+	}
 
+	@Override
+	protected void onStart() {
+		super.onStart();
+		startService(new Intent(cxt, CricScoreService.class));
+		Intent intent = new Intent(cxt, CricScoreService.class);
+		bindService(intent, connection, Context.BIND_AUTO_CREATE);
+		Log.d(TAG, "onStart");
+		if(bound){
+			Log.d(TAG, "cricScoreService.getScores():" + cricScoreAPI.getScoresChanged());
+		}
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	MenuInflater inflater = getSupportMenuInflater();
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		Log.d(TAG, "onCreateOptionsMenu");
+		MenuInflater inflater = getSupportMenuInflater();
 		inflater.inflate(R.menu.main, menu);
 		return super.onCreateOptionsMenu(menu);
-    }
-    
+	}
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -46,4 +61,47 @@ public class MainActivity extends SherlockListActivity {
 			return super.onOptionsItemSelected(item);
 		}
 	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		// Unbind from the service
+		if (bound) {
+			unbindService(connection);
+			bound = false;
+		}
+	}
+
+	/**
+	 * Called when a button is clicked (the button in the layout file attaches
+	 * to this method with the android:onClick attribute)
+	 */
+	/*
+	 * public void onButtonClick(View v) { if (mBound) { // Call a method from
+	 * the LocalService. // However, if this call were something that might
+	 * hang, then this request should // occur in a separate thread to avoid
+	 * slowing down the activity performance. int num =
+	 * mService.getRandomNumber(); Toast.makeText(this, "number: " + num,
+	 * Toast.LENGTH_SHORT).show(); } }
+	 */
+
+	/** Defines callbacks for service binding, passed to bindService() */
+	private ServiceConnection connection = new ServiceConnection() {
+		@Override
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			// We've bound to LocalService, cast the IBinder and get
+			// LocalService instance
+			LocalBinder binder = (LocalBinder) service;
+			cricScoreAPI = binder.getService();
+			bound = true;
+			Log.d(TAG, "connecting to service");
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName arg0) {
+			bound = false;
+			Log.d(TAG, "disconnecting from service");
+		}
+	};
+
 }
