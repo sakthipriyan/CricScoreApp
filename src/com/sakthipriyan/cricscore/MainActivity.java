26b8a67a94b2b4e3b6fdfc6d3c58301c9c75b650
Sakthi Priyan H <sakthipriyans@gmail.com>
2013-07-13 13:44:08 +0530

    Refresh icon is animated instead of progress bar.

--- src/com/sakthipriyan/cricscore/MainActivity.java
+++ src/com/sakthipriyan/cricscore/MainActivity.java
@@ -17,19 +17,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
-import com.actionbarsherlock.view.Window;
 import com.sakthipriyan.cricscore.CricScoreService.CricScoreAPI;
 import com.sakthipriyan.cricscore.CricScoreService.LocalBinder;
 
@@ -39,7 +40,7 @@ public class MainActivity extends SherlockActivity {
 	public static final String UPDATE_STARTED = "com.sakthipriyan.cricscore.UPDATE_STARTED";
 	public static final String UPDATE_COMPLETED = "com.sakthipriyan.cricscore.UPDATE_COMPLETED";
 	private static final String TAG = MainActivity.class.getSimpleName();
-	
+
 	// For local reference
 	private Context cxt;
 	private CricScoreAPI cricScoreAPI;
@@ -47,15 +48,14 @@ public class MainActivity extends SherlockActivity {
 	private CricScoreReceiver receiver;
 	private IntentFilter filter;
 	private List<Score> matches;
-	private List<Score> liveScores;
 	private MatchAdapter matchAdapter;
 	private Set<Integer> liveMatches;
-	private Menu menu;
+	private MenuItem refreshItem;
+	private ImageView iv;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.activity_main);
 		cxt = this;
 		this.receiver = new CricScoreReceiver();
@@ -65,7 +65,6 @@ public class MainActivity extends SherlockActivity {
 		this.matches = new ArrayList<Score>();
 		this.liveMatches = new HashSet<Integer>();
 		this.matchAdapter = new MatchAdapter(cxt);
-		//getSherlock().setProgressBarIndeterminateVisibility(true);
 		Log.d(TAG, "onCreate");
 	}
 
@@ -77,13 +76,13 @@ public class MainActivity extends SherlockActivity {
 		bindService(intent, connection, Context.BIND_AUTO_CREATE);
 		Log.d(TAG, "onStart");
 	}
-	
+
 	@Override
 	protected void onResume() {
 		registerReceiver(receiver, filter);
 		super.onResume();
 	}
-	
+
 	@Override
 	public void onPause() {
 		unregisterReceiver(receiver);
@@ -94,7 +93,7 @@ public class MainActivity extends SherlockActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getSupportMenuInflater();
 		inflater.inflate(R.menu.main, menu);
-		this.menu = menu;
+		this.refreshItem = menu.findItem(R.id.refresh);
 		return super.onCreateOptionsMenu(menu);
 	}
 
@@ -105,7 +104,7 @@ public class MainActivity extends SherlockActivity {
 			startActivity(new Intent(cxt, Settings.class));
 			return true;
 		case R.id.refresh:
-			Toast.makeText(cxt, "Refreshing..",	 Toast.LENGTH_SHORT).show();
+			cricScoreAPI.refresh();
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
@@ -115,89 +114,72 @@ public class MainActivity extends SherlockActivity {
 	@Override
 	protected void onStop() {
 		super.onStop();
-		// Unbind from the service
 		if (bound) {
 			unbindService(connection);
 			bound = false;
 		}
 	}
 
-	/**
-	 * Called when a button is clicked (the button in the layout file attaches
-	 * to this method with the android:onClick attribute)
-	 */
-	/*
-	 * public void onButtonClick(View v) { if (mBound) { // Call a method from
-	 * the LocalService. // However, if this call were something that might
-	 * hang, then this request should // occur in a separate thread to avoid
-	 * slowing down the activity performance. int num =
-	 * mService.getRandomNumber(); Toast.makeText(this, "number: " + num,
-	 * Toast.LENGTH_SHORT).show(); } }
-	 */
-
-	private ServiceConnection connection = new ServiceConnection() {
-		@Override
-		public void onServiceConnected(ComponentName className, IBinder service) {
-			LocalBinder binder = (LocalBinder) service;
-			cricScoreAPI = binder.getService();
-			bound = true;
-			Log.d(TAG, "connecting to service");
+	private void matchClicked(Score score, View view) {
+		Integer id = score.getId();
+		if (liveMatches.contains(id)) {
+			view.setBackgroundResource(0);
+			liveMatches.remove(id);
+			cricScoreAPI.removeMatch(id);
+			//Toast.makeText(cxt, score.getTeam1() + " vs "  + score.getTeam2() + " removed", Toast.LENGTH_LONG).show();
+		} else {
+			view.setBackgroundColor(getResources().getColor(R.color.light_red));
+			liveMatches.add(id);
+			cricScoreAPI.addMatch(id);
+			//Toast.makeText(cxt, score.getTeam1() + " vs "  + score.getTeam2() + " added", Toast.LENGTH_LONG).show();
 		}
-
-		@Override
-		public void onServiceDisconnected(ComponentName arg0) {
-			bound = false;
-			Log.d(TAG, "disconnecting from service");
-		}
-	};
-	
-	private void addMatch(int id){
-		Toast.makeText(cxt, id + "", Toast.LENGTH_LONG).show();
-		cricScoreAPI.addMatch(id);
 	}
-	
-	private void showAllMatches(){
+
+	private void showAllMatches() {
 		this.matches = cricScoreAPI.listMatches();
 		final ListView list = (ListView) findViewById(R.id.matchList);
 		list.setAdapter(matchAdapter);
 		list.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> arg0, View view,
 					int position, long id) {
-				Integer matchId = ((Score)list.getItemAtPosition(position)).getId();
-				addMatch(matchId);
+				Score score = ((Score) list.getItemAtPosition(position));
+				matchClicked(score, view);
 			}
 		});
-		final LinearLayout progress =  (LinearLayout) findViewById(R.id.loadingInProgress);
+		final LinearLayout progress = (LinearLayout) findViewById(R.id.loadingInProgress);
 		progress.setVisibility(View.GONE);
-		final LinearLayout content =  (LinearLayout) findViewById(R.id.content);
+		final LinearLayout content = (LinearLayout) findViewById(R.id.content);
 		content.setVisibility(View.VISIBLE);
-		getSherlock().setProgressBarIndeterminateVisibility(false);
 	}
-	
-	private class CricScoreReceiver extends BroadcastReceiver {		
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			long start = System.currentTimeMillis();
-			if(UPDATE_MATCHES.equals(intent.getAction())){
-				showAllMatches();
-			} else if(UPDATE_STARTED.equals(intent.getAction())){
-				menu.findItem(R.id.refresh).setVisible(false);
-				getSherlock().setProgressBarIndeterminateVisibility(true);
-			} else if(UPDATE_COMPLETED.equals(intent.getAction())){
-				getSherlock().setProgressBarIndeterminateVisibility(false);
-				menu.findItem(R.id.refresh).setVisible(true);
-			}
-			long time = System.currentTimeMillis() - start;
-			Log.i(TAG, "On Receive Time Taken : " + time + " ms");
-		} 
+
+	private void refreshStart() {
+		if (this.iv == null) {
+			LayoutInflater inflater = (LayoutInflater) getApplication()
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			this.iv = (ImageView) inflater.inflate(R.layout.refresh_view, null);
+		}
+
+		Animation rotation = AnimationUtils.loadAnimation(getApplication(),
+				R.anim.rotation_refresh);
+		rotation.setRepeatCount(Animation.INFINITE);
+		this.iv.startAnimation(rotation);
+
+		refreshItem.setActionView(this.iv);
 	}
-	
+
+	private void refreshEnd() {
+		if (this.refreshItem != null
+				&& this.refreshItem.getActionView() != null) {
+			this.refreshItem.getActionView().clearAnimation();
+			this.refreshItem.setActionView(null);
+		}
+	}
+
 	private class MatchAdapter extends BaseAdapter {
 		private LayoutInflater mInflater;
 
 		public MatchAdapter(Context context) {
 			mInflater = LayoutInflater.from(context);
-
 		}
 
 		public int getCount() {
@@ -240,4 +222,37 @@ public class MainActivity extends SherlockActivity {
 			TextView text2;
 		}
 	}
+
+	private class CricScoreReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			long start = System.currentTimeMillis();
+			if (UPDATE_MATCHES.equals(intent.getAction())) {
+				showAllMatches();
+			} else if (UPDATE_STARTED.equals(intent.getAction())) {
+				refreshStart();
+			} else if (UPDATE_COMPLETED.equals(intent.getAction())) {
+				refreshEnd();
+			}
+			long time = System.currentTimeMillis() - start;
+			Log.i(TAG, "On Receive Time Taken : " + time + " ms");
+		}
+	}
+
+	private ServiceConnection connection = new ServiceConnection() {
+		@Override
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			LocalBinder binder = (LocalBinder) service;
+			cricScoreAPI = binder.getService();
+			bound = true;
+			Log.d(TAG, "connecting to service");
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName arg0) {
+			bound = false;
+			Log.d(TAG, "disconnecting from service");
+		}
+	};
+
 }
