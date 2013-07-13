44f611fc233dbdb5e35c3b4378b7637167f055a9
Sakthi Priyan H <sakthipriyans@gmail.com>
2013-07-13 22:28:31 +0530

    Currently testing layout for detailed score. Test will be removed later.

---
+++ src/com/sakthipriyan/cricscore/Test.java
@@ -0,0 +1,15 @@
+package com.sakthipriyan.cricscore;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class Test extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.detail_view);
+	}
+
+}
