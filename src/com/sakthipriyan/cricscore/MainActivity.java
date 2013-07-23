6da2a3055e6862c6fd77ec7091e859b599c35b2c
Sakthi Priyan H <sakthipriyans@gmail.com>
2013-07-23 23:05:44 +0530

    Removed unused files. Minor modifications

--- src/com/sakthipriyan/cricscore/MainActivity.java
+++ src/com/sakthipriyan/cricscore/MainActivity.java
@@ -11,7 +11,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
-import android.opengl.Visibility;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
